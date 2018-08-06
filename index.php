dada39c71bf6f15ac14d11ee15f0ea0e882fb870
Kio <Kio.thedev@gmail.com>
2018-08-06 14:37:16 -0400

    Added first page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Cover Template for Bootstrap</title>
+    <title>Legend of Ikaros</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
