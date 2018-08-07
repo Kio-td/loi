15965dcbc305aae8a43a0a915407a90f93de81ce
Kio <Kio.thedev@gmail.com>
2018-08-07 10:42:40 -0400

    Added Cryptogrophy

--- base/head.php
+++ base/head.php
@@ -5,6 +5,7 @@
     <meta name="author" content="">
 
     <title>Legend of Ikaros</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/aes-min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
     <link href="/cover.css" rel="stylesheet">
     <link rel="author" href="humans.txt" />
--- game/login.php
+++ game/login.php
@@ -27,13 +27,31 @@ if (isset($_GET["reg"])) {
   <main role="main" class="inner cover">
   <h1 class="cover-heading">Registration</h1>
   <p class="lead">Register your existance with the department.</p>
-  <form action="?auth&register" method="post">
+  <form id="reg" action="?auth&register" method="post">
     <input class="form-control" name="un" placeholder="Username" required="" type="text"><br>
-    <input class="form-control" type="password" name="pw" required placeholder="Password"><br>
+    <input class="form-control" id="pw" type="password" name="pw" required placeholder="Password"><br>
     <input class="form-control" type="email" name="em" required placeholder="Email"><br>
+    <input type="hidden" name="salt">
+    <input type="hidden" name="iv">
     <input class="btn btn-secondary" type="submit" value="Register">
+    <button onclick="register()" class="btn btn-secondary">register</button>
   </form>
 
+  <script>
+  function register() {
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+text = ""
+for (let i = 0; i < 40; i++)
+text += possible.charAt(Math.floor(Math.random() * possible.length));
+iv = ""
+for (let i = 0; i < 40; i++)
+iv += possible.charAt(Math.floor(Math.random() * possible.length));
+  antex =  CryptoJS.AES.encrypt(document.getElementById('pw').value, text, {iv: iv});
+  document.getElementById('pw').value = antex.ciphertext.toString(lib_crypt.CryptoJS.enc.Base64);
+  document.getElementById('salt').value = text;
+  document.getElementById('iv').value = iv;
+  }
+</script>
 
   <?php
 
@@ -47,7 +65,10 @@ if (isset($_GET["reg"])) {
 <main role="main" class="inner cover">
 <h1 class="cover-heading">Login</h1>
 <p class="lead">Welcome back. Login to continue your adventure.</p>
-
+<form id="log" action="?auth" method="post">
+  <input class="form-control" name="un" placeholder="Username" required type="text"><br>
+  <input class="form-control" name="pw" placeholder="Password" required type="password"><br>
+  <button onclick="login()" class="btn btn-secondary">Login</button>
 <?php
 
 }
