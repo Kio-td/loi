2b56bfcd7f852711f824c9091aea59edbe2dac51
Kio <Kio.thedev@gmail.com>
2018-08-06 21:01:26 -0400

    Work on the index.php page

---
+++ game/index.php
@@ -0,0 +1,13 @@
+<?php
+require('../base/head.php'); ?>
+<a class="nav-link" href="login.php">Play</a>
+<a class="nav-link" href="login.php?reg">Register</a>
+</nav>
+</div>
+</header>
+
+<main role="main" class="inner cover">
+<h1 class="cover-heading">You're not logged in.</h1>
+<p class="lead">Press the button if you'd like to continue your quest.</p>
+
+<?php require('../base/feet.php'); ?>
