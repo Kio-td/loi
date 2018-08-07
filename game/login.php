9557081ef27cd4bc06e14549a453c1302275396c
Kio <Kio.thedev@gmail.com>
2018-08-07 09:54:48 -0400

    And the email

--- game/login.php
+++ game/login.php
@@ -25,7 +25,8 @@ if (isset($_GET["reg"])) {
   <h1 class="cover-heading">Registration</h1>
   <p class="lead">Register your existance with the department.</p>
   <form action="?auth&register" method="post">
-    <input type="text" name="un" placeholder="Username">&emsp;<input type="password" name="pw" placeholder="Password">
+    <input type="text" name="un" placeholder="Username">&emsp;<input type="password" name="pw" placeholder="Password"><br>
+    <input type="email" name="em" placeholder="Email">
   </form>
 
 
