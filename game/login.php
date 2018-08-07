6f58491f6d91b3e19af0668d4d568eafa7c9d795
Kio <Kio.thedev@gmail.com>
2018-08-06 21:13:24 -0400

    a

--- game/login.php
+++ game/login.php
@@ -3,7 +3,7 @@
 if (isset($_GET["auth"])) {
     require('/var/www/no-access/loi/config.php');
   if (isset($_GET["register"])) {
-    
+
 
   }
 
@@ -14,7 +14,19 @@ require('../base/head.php');
 
 if (isset($_GET["reg"])) {
 
+  ?>
+  <a class="nav-link" href="login.php">Login</a>
+  <a class="nav-link active" href="login.php?reg">Register</a>
+  </nav>
+  </div>
+  </header>
 
+  <main role="main" class="inner cover">
+  <h1 class="cover-heading">You're not logged in.</h1>
+  <p class="lead">Use the links up top if you'd like to continue your quest.</p>
+
+
+  <?php
 
 } else {
 
