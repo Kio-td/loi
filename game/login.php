506866e2100021b10c712256b875ee545d9718c8
Kio <Kio.thedev@gmail.com>
2018-08-06 21:10:59 -0400

    Began to work on login script

---
+++ game/login.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_GET["auth"])) {
+    require('/var/www/no-access/loi/config.php');
+  if (isset($_GET["register"])) {
+    
+
+  }
+
+
+}
+
+require('../base/head.php');
+
+if (isset($_GET["reg"])) {
+
+
+
+} else {
+
+
+}
+
+require('../base/feet.php');
+
+ ?>
