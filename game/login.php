f8a9579ef8a1a0a5772c422efc53585829b5929f
Kio <Kio.thedev@gmail.com>
2018-08-07 10:11:32 -0400

    Began Login page, and finished Registration layout

--- game/login.php
+++ game/login.php
@@ -29,13 +29,21 @@ if (isset($_GET["reg"])) {
     <input class="form-control" name="un" placeholder="Username" required="" type="text"><br>
     <input class="form-control" type="password" name="pw" required placeholder="Password"><br><br>
     <input class="form-control" type="email" name="em" required placeholder="Email">
+    <input class="btn btn-lg btn-secondary" type="submit" value="Register">
   </form>
 
 
   <?php
 
 } else {
+?>
+<a class="nav-link active" href="login.php">Login</a>
+<a class="nav-link" href="login.php?reg">Register</a>
+</nav>
+</div>
+</header>
 
+<?php
 
 }
 
