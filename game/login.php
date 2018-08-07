ed4e39db98716f48bd93b7b93580f80f72478b0b
Kio <Kio.thedev@gmail.com>
2018-08-07 11:02:28 -0400

    Added blacklist

--- game/login.php
+++ game/login.php
@@ -6,6 +6,16 @@ if (isset($_GET["auth"])) {
 
 
   } else {
+    $upl = array(
+      "username" => $_POST["un"],
+      "password" => password_hash($_POST["pw"]),
+      "email" => $_POST["em"]
+    );
+    $blacklist = array("ikaros", "admin", "console", "sysadmin", "owner", "dev", "developer", "support", "superuser", "root", "system", "bot", "npc");
+    if(in_array(strtolower($upl["username"]), $blacklist)) {
+      header("Location: login.php?reg&x=1");
+    }
+
 
   }
 
@@ -13,44 +23,38 @@ if (isset($_GET["auth"])) {
 }
 echo "<style>input{color: white !important;}</style>";
 require('../base/head.php');
-
-
-if (isset($_GET["reg"])) {
-
   ?>
   <a class="nav-link" href="login.php">Login</a>
   <a class="nav-link active" href="login.php?reg">Register</a>
   </nav>
   </div>
   </header>
-
   <main role="main" class="inner cover">
+
+    <?php
+    if (isset($_GET["reg"])) {
+      if(isset($_GET["x"])) {
+        echo '<div class="alert alert-danger" role="alert">';
+        switch ($_GET["x"]) {
+          case '1':
+          echo ""
+            break;
+
+          default:
+            // code...
+            break;
+        }
+      }
+      echo "</div>";
+     ?>
   <h1 class="cover-heading">Registration</h1>
   <p class="lead">Register your existance with the department.</p>
   <form id="reg" action="?auth&register" method="post">
     <input class="form-control" name="un" placeholder="Username" required="" type="text"><br>
     <input class="form-control" id="pw" type="password" name="pw" required placeholder="Password"><br>
     <input class="form-control" type="email" name="em" required placeholder="Email"><br>
-    <input type="hidden" name="salt">
-    <input type="hidden" name="iv">
-    <button onclick="register()" class="btn btn-secondary">register</button>
+    <button class="btn btn-secondary">register</button>
   </form>
-
-  <script>
-  function register() {
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-text = ""
-for (let i = 0; i < 40; i++)
-text += possible.charAt(Math.floor(Math.random() * possible.length));
-iv = ""
-for (let i = 0; i < 40; i++)
-iv += possible.charAt(Math.floor(Math.random() * possible.length));
-  antex =  CryptoJS.AES.encrypt(document.getElementById('pw').value, text, {iv: iv});
-  document.getElementById('pw').value = antex.ciphertext.toString(lib_crypt.CryptoJS.enc.Base64);
-  document.getElementById('salt').value = text;
-  document.getElementById('iv').value = iv;
-  }
-</script>
 
   <?php
 
@@ -67,7 +71,7 @@ iv += possible.charAt(Math.floor(Math.random() * possible.length));
 <form id="log" action="?auth" method="post">
   <input class="form-control" name="un" placeholder="Username" required type="text"><br>
   <input class="form-control" name="pw" placeholder="Password" required type="password"><br>
-  <button onclick="login()" class="btn btn-secondary">Login</button>
+  <button class="btn btn-secondary">Login</button>
 <?php
 
 }
