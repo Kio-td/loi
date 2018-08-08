f4c1f90014ea9c8928c9c5135869ff2d90fb7b2a
Kio <Kio.thedev@gmail.com>
2018-08-07 21:34:47 -0400

    location was spelt wrong. -w-

--- game/login.php
+++ game/login.php
@@ -24,8 +24,7 @@ if (isset($_GET["auth"])) {
       header("Location: login.php?reg&x=1");
     }
     $x = $conn->query("select username from users where username = '" . $upl["username"] . "'");
-    die(var_dump($x->num_rows));
-    if($conn->query("select username from users where username = '" . $upl["username"] . "'")->num_rows) {header("localtion: login.php?reg&x=4");}
+    if($x->num_rows) {header("Location: login.php?reg&x=4");}
 
     $conn->query("INSERT INTO `users`(`username`, `password`, `email`, `token`, `ce`) VALUES ('".$upl["username"]."','".$upl["password"]."','".$upl["email"]."', '".$upl["token"]."', '".$upl["cfe"]."')");
     require('../base/head.php');
