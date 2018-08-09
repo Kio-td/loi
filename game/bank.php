b39d0663642ce9f685b864e7adad8e321e899de6
Kio <Kio.thedev@gmail.com>
2018-08-09 12:46:31 -0400

    Transaction bones

--- game/bank.php
+++ game/bank.php
@@ -7,10 +7,26 @@ require("../base/head.php");
  </div>
  </header>
 
+
+<?
+if (isset($_GET["transfer"])) {
+  ?>
+ <main role="main" class="inner cover">
+   <h1 class="cover-heading">Transaction info-</h1>
+   <form method="post">
+     <input type="text" class="form-control" name="un" placeholder="Username"><br>
+     <input type="number" min="1" max="<?php echo $bal; ?>" placeholder="Amount"><br>
+     <input type="submit" class="btn btn-secondary" value="Send">
+   </form>
+} else {
+
+
+?>
  <main role="main" class="inner cover">
    <h1 class="cover-heading">Good day, <?php echo ucfirst($username); ?>.</h1>
    <p class="lead">Welcome to the bank. Your current account balance: <?php echo $bal; ?>Tn.</p>
    <p class="lead">Options: <a href="?transfer">Transfer</a></p>
 
 <?php
+}
 require("../base/feet.php");?>
