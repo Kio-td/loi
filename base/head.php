36b43d85cfc79c811346b6b92a13e442bee87eeb
Kio <Kio.thedev@gmail.com>
2018-08-09 12:12:05 -0400

    Add balance.

--- base/head.php
+++ base/head.php
@@ -17,7 +17,7 @@ $auth = false;
 if (isset($_COOKIE["token"])) {
   require("/var/www/no-access/loi/config.php");
   $n = $conn->escape_string(base64_decode($_COOKIE["token"]));
-  $c = $conn->query("SELECT token, username from users where token = '".$n."'");
+  $c = $conn->query("SELECT bal, token, username from users where token = '".$n."'");
   if (!$c->num_rows) {
     $na = array("index.php", "login.php");
     if(in_array(basename($_SERVER['PHP_SELF'], $na))) {header("Location: index.php");}
@@ -25,6 +25,7 @@ if (isset($_COOKIE["token"])) {
   $auth = true;
   $c = $c->fetch_assoc();
   $username = $c["username"];
+  $bal = $c["bal"];
 }
 }
 
