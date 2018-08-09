c762edeeba6a6ebf6ed12e722bacfcfa58967711
Kio <Kio.thedev@gmail.com>
2018-08-09 09:48:50 -0400

    Fixed typo

--- base/head.php
+++ base/head.php
@@ -17,8 +17,7 @@ try {
 if (isset($_COOKIE["token"])) {
   require("/var/www/no-access/loi/config.php");
   $n = $conn->escape_string(base64_decode($_COOKIE["token"]));
-  $c = $conn->query("SELECT token, user from users where token = '".$n."'");
-  die($conn->error);
+  $c = $conn->query("SELECT token, username from users where token = '".$n."'");
   if (!$c->num_rows) {
     $na = array("index.php", "login.php");
     if(in_array(basename($_SERVER['PHP_SELF'], $na))) {header("Location: index.php");}
