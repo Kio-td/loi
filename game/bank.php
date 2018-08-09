2912cd3f28e9d27fd3b7341a73df1f8c938b8cb7
Kio <Kio.thedev@gmail.com>
2018-08-09 12:09:52 -0400

    o~O

--- game/bank.php
+++ game/bank.php
@@ -6,3 +6,7 @@ require("../base/head.php");
  </nav>
  </div>
  </header>
+
+ <main role="main" class="inner cover">
+   <h1 class="cover-heading">Good day, <?php echo ucfirst($username); ?></h1>
+   <p class="lead">Current account balance: <?php ?></p>
