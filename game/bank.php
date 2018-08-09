609291465855fef30ca41de81c5945a38429c523
Kio <Kio.thedev@gmail.com>
2018-08-09 12:14:16 -0400

    Shouldn't be active.

--- game/bank.php
+++ game/bank.php
@@ -1,7 +1,7 @@
 <?php
 require("../base/head.php");
  ?>
- <a class="nav-link active" href="index">Home</a>
+ <a class="nav-link" href="index">Home</a>
  <a class="nav-link" href="login?lo">Logout</a>
  </nav>
  </div>
