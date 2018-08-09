c1d9a5e5e37e6b93ac85d7aae2f97730516f0012
Kio <Kio.thedev@gmail.com>
2018-08-09 14:26:43 -0400

    Fixed it, yay.

--- game/bank.php
+++ game/bank.php
@@ -59,7 +59,6 @@ if (isset($_GET["transfer"])) {
         $ticket = uniqid("tfr_");
         $conn->query("UPDATE `users` SET `bal`=`bal` - ".($no + ceil($no * 0.15))." WHERE `username` = '".$username."'");
         $conn->query("UPDATE `users` SET `bal`=`bal` + ".$no." WHERE `username` = '".$x."'");
-        die($conn->error);
         $conn->query("INSERT INTO `ticket`(`tid`, `ufrom`, `uto`, `amnt`) VALUES ('$ticket', '$username', '$x', '$no')");
         ?>
           <h1 class="cover-heading">Sent</h1>
