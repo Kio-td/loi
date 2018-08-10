5d44a5ea85c0192f1b795ebac6f7106add97e672
Kio <Kio.thedev@gmail.com>
2018-08-10 08:11:00 -0400

    OWO

--- game/reset.php
+++ game/reset.php
@@ -22,7 +22,7 @@ if($auth) {
 <h1 class="cover-heading">Reset Password</h1>
 <p class="lead">Please put your email below and we will send you a link to reset your account.</p>
 <form method="post">
-  <input class="form-control" name="eml" type="email" required>
+  <input class="form-control" name="eml" type="email" required><br>
   <input type="submit" class="btn btn-secondary" value="Reset">
 </form>
 <?php } require('../base/feet.php');?>
