babecae00722eac013d389662f6e8cdf36ffa241
Kio <Kio.thedev@gmail.com>
2018-08-22 09:36:27 -0400

    18+.

---
+++ 18.php
@@ -0,0 +1,9 @@
+<?php require('./base/head.php'); ?>
+            <a class="nav-link" href="/">Home</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">18 and Over.</h1>
+        <p class="lead">This website contains age-restricted materials.<br>If you are under the age of 18 years, or under the age of majority in the location from where you are accessing this website you do not have authorization or permission to enter this website or access any of its materials.<br>If you are over the age of 18, then clicking "play", or "Play now", will bind you to the terms of service.<br>You also acknowledge that if you press the buton, you are not offended by great deals of explination regarding the death of characters.<br>By clicking on the "Enter" button, and by entering this website you agree with all the above and certify under penalty of perjury that you are an adult.</p>
--- index.php
+++ index.php
@@ -13,4 +13,9 @@
           <a href="game/" class="btn btn-lg btn-secondary">Play now</a>
         </p>
 
+        <footer class="mastfoot mt-auto">
+          <div class="inner">WARNING: This game contains material that will scar you for life.<br>Please read the full <a href="18.php">18+ service</a>.</div>
+        </footer>
+        </div>
+
 <?php require('./base/feet.php'); ?>
