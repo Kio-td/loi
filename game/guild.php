3237bf98e3899ae0abe8e16e820578e567c174a9
Kio <Toobanooba@gmail.com>
2018-08-22 12:52:56 -0400

    UGH

--- game/guild.php
+++ game/guild.php
@@ -14,11 +14,12 @@ if($p["guild"] == 0 ) {
     if ($bal >= $mg) {
 
     } else {
-      echo "<div class='alert alert-danger'>You need ". $mg - $bal . "Tn. more to create a guild.</div>";
+    $n = 1;
     }
   }
   ?>
   <main role="main" class="inner cover">
+    <?php if (!$n == undefined) {  echo "<div class='alert alert-danger'>You need ". $mg - $bal . "Tn. more to create a guild.</div>";}?>
     <h1 class="cover-heading">Guildmaster's home</h1>
     <p class="lead">Please choose an option.</p>
     <p class="lead"><a href="?c">Register a Guild</a>&emsp;<a href="?j">Join a Guild</a></p>
