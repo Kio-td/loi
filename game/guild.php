e439f63296fb217c0ccb5ed75386d977dea5639b
Kio <Toobanooba@gmail.com>
2018-08-31 08:29:49 -0400

    Maxlength

--- game/guild.php
+++ game/guild.php
@@ -18,7 +18,7 @@ if($p["guild"] == 0 ) {
           <p class="lead">This will cost <?php echo $mg; ?>.</p>
           <form>
             <input type="text" name="gid" class="form-control" placeholder="Guild Name"><br>
-            <input type="text" id="tag1" name="sg" class="form-control" oninput="tr()" placeholder="Guild tag (Max 4 Characters)">
+            <input type="text" id="tag1" maxlength="4" name="sg" class="form-control" oninput="tr()" placeholder="Guild tag (Max 4 Characters)">
             <p class="vg">Your Guild tag will look like this:<br>
             <span id="tag"></span><?php echo $username; ?></p>
 
