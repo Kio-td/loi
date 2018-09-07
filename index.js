90c0bd17d2b8bbd1e0f60eefb627c4b02041e3cf
Kio <Toobanooba@gmail.com>
2018-09-07 17:21:33 -0400

    Haul in the !f [friend]

--- index.js
+++ index.js
@@ -99,8 +99,10 @@ con.connect(function(err) {
 	chat.on('connection', function connection(ws, req) {
 		isconnected(req, ws);
 		ws.on('message', function msg(data) {
+			let ip = req.connection.remoteAddress.replace(/::ffff:/g, '');
+			let uid = ip.replace(/\./g, '');
 			ds = 0
-			data = data.split(/\r?\n|\r/g)[0].replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+			data = data.split(/\r?\n|\r/g)[0].replace(/\</g, '&lt;').replace(/\>/g, '&gt;').trim();
 			if (data.length >= 80) {
 				ws.send(json.stringify({ ok: false, display: "*Your voice falls on deaf ears. (Too many characters.)", color: "red" }));
 				ds = 1
@@ -132,7 +134,22 @@ con.connect(function(err) {
 							});
 						});
 					} else {
-						//Check for online friend and check if online.
+						fun = data.split(" ")[1]
+
+						con.query("select uid from users where token = ?", [cfg.get("user." + uid + ".token")], function(f, g) {
+							con.query("select token from users where username = ? and uid in ( select ut from friends where uf = ? ) or uid in ( select uf from friends where ut = ? )", [fun, g[0].uid, g[0].uid], function(a, b) {
+								if (b.length == 0) { ws.send(json.stringify({ ok: false, display: fun + " is not a friend of yours. Are you sure you typed their name correctly?" })) }
+								chat.clients.forEach(function each(client) {
+									if (client.readyState === WebSocket.OPEN) {
+										con.query("SELECT citid from users where token = ?", [cfg.get("user." + client._socket.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '') + ".token")], function a(a, b) {
+											uid = req.connection.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '');
+											if (a) throw a;
+											if (b.length == 1) { client.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f all ", ""), color: "pink" })); }
+										});
+									}
+								});
+							});
+						});
 					}
 				} else if (data.split(" ")[0] == "!p") {
 					//Party
