5efe230377a65d0b95e6727c9f6cb8bbf9706940
Kio-td <toobanooba@gmail.com>
2018-09-12 15:08:50 -0400

    Ugh

--- base/head.php
+++ base/head.php
@@ -1,5 +1,18 @@
 <?php
-require('/var/www/html/loi/base/head/smail.php');
+function sendmail($sub, $mail, $user, $body) {
+require '/var/www/loi/vendor/autoload.php';
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("Noreply@loi.nayami.party", "Legend of Ikaros");
+$email->setSubject($sub);
+$email->addTo($mail, $user);
+$email->addContent("text/plain", $body);
+$sendgrid = new \SendGrid("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");
+try {
+    $response = $sendgrid->send($email);
+} catch (Exception $e) {
+    echo 'We were unable to send your email. Please, notify Kio.';
+}
+}
 $auth = false;
 if (isset($_COOKIE["token"])) {
   require("/var/www/no-access/loi/config.php");
@@ -16,5 +29,25 @@ if (isset($_COOKIE["token"])) {
 }
 }
 $x = false;
-require('/var/www/html/loi/base/head/temp.php');
 ?>
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Legend of Ikaros</title>
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+    <link href="/cover.css" rel="stylesheet">
+    <link rel="author" href="humans.txt" />
+  <style>#content > #right > .dose > .dosesingle,
+#content > #center > .dose > .dosesingle
+{display:none !important;}</style></head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+            <h3 class="masthead-brand">Legend of Ikaros</h3>
+            <nav class="nav nav-masthead justify-content-center">
