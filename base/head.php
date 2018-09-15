e565bebfda39fde8fe7c41982efd15a2208273b4
Kio-td <toobanooba@gmail.com>
2018-09-15 01:42:46 -0400

    Let's do a massive websocket switch And remove the auth page on the way.

--- base/head.php
+++ base/head.php
@@ -36,6 +36,7 @@ $x = false;
     <meta name="description" content="">
     <meta name="author" content="">
     <script>
+    json = JSON5;
       function err(info) {
         document.getElementById('ap').innerHTML += '<div class="alert alert-warning alert-dismissible fade show" role="alert">'+info+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
       }
--- game/login-.php
+++ game/login-.php
@@ -21,58 +21,10 @@ if (isset($_GET["confirm"])) {
       die();
     }
   }
-
-
 }
 elseif (isset($_GET["lo"])) {
   setcookie("token", '' , time() - 3600);
   header("Location: index");
-}
-elseif (isset($_GET["auth"])) {
-      if(isset($_POST["un"]) && isset($_POST["pw"])) {
-        //Username and password are present
-        require("/var/www/no-access/loi/config.php");
-        $u = $conn->escape_string(strtolower($_POST["un"]));
-        $p = $conn->escape_string($_POST["pw"]);
-        $n = $conn->query("SELECT password, ce, token from users where username = '".$u."'");
-        if ($n->num_rows) {
-          //User is present
-          $r = $n->fetch_assoc();
-          if (password_verify($p, $r["password"])) {
-            if ($r["ce"] == "0") {
-              //User can be fully authenticated
-              setcookie("token", base64_encode($r["token"]));
-              header("Location: index");
-              require('../base/head.php');
-              ?>
-              <a class="nav-link" href="index">Home</a>
-            </nav>
-          </div>
-          </header>
-          <main role="main" class="inner cover">
-            <h1 class="cover-heading">Logged in</h1>
-            <p class="lead">You are now being redirected back to the game...</p>
-              <?php
-              require('../base/feet.php');
-            } else {
-              //User has uncomfirmed email
-              header("Location: login?x=2");
-            }
-          } else {
-            //Password is false
-            header("Location: login?x=1");
-          }
-        } else {
-          //User is not present
-          header("Location: login?x=1");
-        }
-
-      } else {
-        //Username and password are not present
-        header("Location: login?x=1");
-        die();
-      }
-
 } else {
     echo "<style>input{color: white !important;}</style>";
     require('../base/head.php');
@@ -84,30 +36,6 @@ elseif (isset($_GET["auth"])) {
 </div>
 </header>
 <main role="main" class="inner cover">
-  <?php
-        if (isset($_GET["x"])) {
-            echo '<div class="alert alert-danger" role="alert">';
-            switch ($_GET["x"]) {
-                case '1':
-                    echo "You attempted to use a blacklisted username.";
-                    break;
-                case '2':
-                    echo "Your password is not long enough.";
-                    break;
-                case '3':
-                    echo "Require is there for a reason.";
-                    break;
-                case '4':
-                    echo "The username chosen is not available.";
-                    break;
-                case '5':
-                    echo "The username is beyond 30 characters.";
-            }
-            echo "</div>";
-        }
-
-?>
-
   <h1 class="cover-heading">Registration</h1>
   <p class="lead">Register your existance with the department.</p>
     <input class="form-control" id="un" placeholder="Username" required="" onfocus="us()" oninput="cun(this)" type="text"><br>
@@ -117,7 +45,6 @@ elseif (isset($_GET["auth"])) {
     <button onclick="sub()" class="btn btn-secondary">register</button>&emsp;<a href="/game/reset">Reset Password</a>
 <span id="info" style="display: none"></span>
 <script>
-json = JSON5;
 var s = new WebSocket("wss://loi.nayami.party:2053/anon");
 data = "";
 i = 0
@@ -201,29 +128,30 @@ function sub() {
 </div>
 </header>
 <main role="main" class="inner cover">
-  <?php
-  if(isset($_GET["x"])) {
-  switch ($_GET["x"]) {
-    case '0':
-      echo "<div class='alert alert-success' role='alert'>Your account has been activated.</div>";
-      break;
-    case '1':
-      echo "<div class='alert alert-danger' role='alert'>The information provided didn't match our records.<br>
-  Need a <a href='reset'>reset</a>?</div>";
-      break;
-    case '2':
-      echo "<div class='alert alert-danger' role='alert'>Please confirm your email.</div>";
-      break;
-  }
-}
-   ?>
+
   <h1 class="cover-heading">Login</h1>
   <p class="lead">Welcome back. Login to continue your adventure. </p>
-  <form id="log" action="?auth" method="post">
-    <input class="form-control" name="un" placeholder="Username" required type="text"><br>
-    <input class="form-control" name="pw" placeholder="Password" required type="password"><br>
-    <button class="btn btn-secondary">Login</button>
-  </form>
+    <input class="form-control" id="un" placeholder="Username" required type="text"><br>
+    <input class="form-control" id="pw" placeholder="Password" required type="password"><br>
+    <button onclick="auth()" class="btn btn-secondary">Login</button>
+
+    <script>
+      s = new WebSocket("wss://loi.nayami.party:2053/anon");
+      function auth() {
+        s.onmessage = function(evt) {
+          data = json.parse(evt.data);
+          if(data.ok == false) {
+            if(data.msg == "CONF_EMAIL") {err("Please look in your inbox for the confirmation email.")}
+            else if (data.msg == "INC_PASS") {err("Your username or password is incorrect.")}
+            else if (data.msg == "NOBODY_FOUND") {err("Your username or password is incorrect.")}
+          } else {
+            document.cookie = "token=" + data.data;
+            window.location = "index";
+          }
+        }
+        s.send(json.stringify({cmd:'auth', data:{un:document.getElementById('un').value, pw:getElementById('pw').value}}))
+      }
+    </script>
     <?php
 
     }
