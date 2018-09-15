ba951a537995470dfa81c5d04d72117b0581757f
Kio-td <toobanooba@gmail.com>
2018-09-15 01:49:28 -0400

    added to that one list of issuous pages

--- base/head.php
+++ base/head.php
@@ -19,8 +19,8 @@ if (isset($_COOKIE["token"])) {
   $n = $conn->escape_string(base64_decode($_COOKIE["token"]));
   $c = $conn->query("SELECT bal, token, username from users where token = '".$n."'");
   if (!$c->num_rows) {
-    $na = array("index.php", "login.php");
-    if(in_array(basename($_SERVER['PHP_SELF']), $na)) {header("Location: index.php");}
+    $na = array("index.php", "login.php", "index", "login", "login-");
+  //  if(in_array(basename($_SERVER['PHP_SELF']), $na)) {header("Location: index.php");}
 } else {
   $auth = true;
   $c = $c->fetch_assoc();
