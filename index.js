3f8eebe0036bdb156e33ad4fd494ca62626930cc
Kio-td <toobanooba@gmail.com>
2018-10-18 13:20:51 -0400

    Taking it by the book

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const url = require('url');
 const cfg = new cfg1("./config/local.json"); //Configuration for Server, and for users
 const mcg = new cfg1("./config/hvw.json"); //HVW - High-Velocity Writing - This function will most likely be written to A LOT. It's important to make sure that the API will always be ready.
 const g = cfg.get('int.websock');
-const uidgen = require('uid-generator');
+const UIDGenerator = require('uid-generator');
 const fs = require('fs');
 const server = http.createServer({});
 const serve = new WebSocket.Server(g);
@@ -18,7 +18,7 @@ const battle = new WebSocket.Server(g);
 const anon = new WebSocket.Server(g);
 const black = ["ikaros", "admin", "console", "sysadmin", "owner", "dev", "developer", "support", "superuser", "root", "system", "bot", "npc"];
 
-const uid = new uidgen();
+const uidgen = new UIDGenerator();
 
 mail.setApiKey(cfg.get("int.sg"));
 
@@ -124,7 +124,7 @@ con.connect(function(err) {
 								if (a) throw a;
 								if (b.length != 1) {ws.send(json.stringify({ok:false, code:-3, msg:"NO_USR"}))}
 								else {
-									token = uid.generateSync();
+									token = uidgen.generateSync();
 									con.query("update users set rs=? where username=?", [token, d["data"]], function(a) {
 										if (a) throw a;
 									});
@@ -167,8 +167,8 @@ con.connect(function(err) {
 						if(b.length > 0) {ws.send(json.stringify({ok: false, code:6, msg: "ACCT_EXISTS"}))}
 						else if (black.includes(n.un.toLowerCase()))  {ws.send(json.stringify({ok: false, code:6, msg: "ACCT_BLACKLIST"}))}
 						else {
-							token = uid.generateSync();
-						  ce = uid.generateSync();
+							token = uidgen.generateSync();
+						  ce = uidgen.generateSync();
 							con.query("INSERT INTO `users`(`username`, `password`, `email`, `token`, `ce`, `spid`) VALUES (?,?,?,?,?,?);", [n.un, pass.hash(n.pw),n.em, token, ce, n.sp], function (a) {
 								if (a) throw a;
 								sendemail(n.em, "d-01419621eb244bd29bb43c34fcd6b5dd", {username: n.un, url: "https://loi.nayami.party/game/login?confirm=" + ce + "&username=" + n.un});
