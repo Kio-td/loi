d9aaee5018fe19f812fa162fa10ad43ce3abc88d
Kio-td <toobanooba@gmail.com>
2018-10-22 16:54:32 -0400

    Finish handling Preemptive Disconnects

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ const black = ["ikaros", "admin", "console", "sysadmin", "owner", "dev", "develo
 
 mail.setApiKey(cfg.get("int.sg"));
 
-function pdc(con, error, ip) {
-	con.query("INSERT INTO `pagelog`(`eid`, `ip`, `page`, `toe`) VALUES (?,?,?,?)", ["SE_"+uuid(13), ip, json.stringify(error), Date.now()]);
+function pdc(error, con, ip) {
+	if (error != undefined) {
+		con.query("INSERT INTO `pagelog`(`eid`, `ip`, `page`, `toe`) VALUES (?,?,?,?)", ["SE_"+uuid(13), ip, json.stringify(error), Date.now()]);
+	}
 }
 
 function sendemail(to, template, data) {
@@ -51,137 +53,137 @@ con.connect(function(err) {
 	anon.on('connection', function(ws, req) {
 		let ip = req.headers['x-forwarded-for'];
 		let uid = ip.replace(/\./g, '');
-			ws.send(json.stringify({ok:true, code:3, msg:"HI_ANON"}));
-			ws.on('message', function(data) {
-				try {
-					d = json.parse(data);
-				} catch (e) {
-					ws.send(json.stringify({ok:false, code:-3, msg:"NOT_JSON5"}));
-					return;
-				}
-				if (d["cmd"] == undefined) {
-					ws.send(json.stringify({ok:false, code:-1, msg:"NO_CMD"}));
-					return;
-				} else if (d["cmd"] == "species") {
-					con.query("select * from spec", function(a,b) {
-						if(a) throw a;
-						ws.send(json.stringify({ok:true, code:4, data:b}));
-					});
-				} else if (d["cmd"] == "auth") {
-					if(d["data"] == undefined) {
-						ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}));
-					} else {
-						data = d["data"]
-						//un, pw
-						data.un = data.un.toLowerCase();
-						data.pw = data.pw;
-						con.query("select ce, password, token from users where username = ?", [data.un], function(a,b) {
-							if (a) throw a;
-							if(b.length == 0) {ws.send(json.stringify({ok:false, code:-4, msg:"NOBODY_FOUND"}))} else {
+		ws.send(json.stringify({ok:true, code:3, msg:"HI_ANON"}), pdc(error, con, ip));
+		ws.on('message', function(data) {
+			try {
+				d = json.parse(data);
+			} catch (e) {
+				ws.send(json.stringify({ok:false, code:-3, msg:"NOT_JSON5"}), pdc(error, con, ip));
+				return;
+			}
+			if (d["cmd"] == undefined) {
+				ws.send(json.stringify({ok:false, code:-1, msg:"NO_CMD"}), pdc(error, con, ip));
+				return;
+			} else if (d["cmd"] == "species") {
+				con.query("select * from spec", function(a,b) {
+					if(a) throw a;
+					ws.send(json.stringify({ok:true, code:4, data:b}), pdc(error, con, ip));
+				});
+			} else if (d["cmd"] == "auth") {
+				if(d["data"] == undefined) {
+					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}), pdc(error, con, ip));
+				} else {
+					data = d["data"]
+					//un, pw
+					data.un = data.un.toLowerCase();
+					data.pw = data.pw;
+					con.query("select ce, password, token from users where username = ?", [data.un], function(a,b) {
+						if (a) throw a;
+						if(b.length == 0) {ws.send(json.stringify({ok:false, code:-4, msg:"NOBODY_FOUND"}), pdc(error, con, ip))} else {
 							s = b[0];
-							if(s.ce !== "0") {ws.send(json.stringify({ok:false, code:-4, msg:"CONF_EMAIL"}));}
+							if(s.ce !== "0") {ws.send(json.stringify({ok:false, code:-4, msg:"CONF_EMAIL"}), pdc(error, con, ip));}
 							else if(pass.verify(data.pw, s.password) == false) {
-								ws.send(json.stringify({ok:false, code:-4, msg:"INC_PASS"}));
+								ws.send(json.stringify({ok:false, code:-4, msg:"INC_PASS"}), pdc(error, con, ip));
 							} else {
-								ws.send(json.stringify({ok:true, code:4, data:Buffer.from(s.token).toString('base64')}));
+								ws.send(json.stringify({ok:true, code:4, data:Buffer.from(s.token).toString('base64')}), pdc(error, con, ip));
 							}
 						}
-						})
+					})
+				}
+			} else if (d["cmd"] == "ress2") {
+				if(d["data"] == undefined) {
+					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}), pdc(error, con, ip));
+				} else {
+					try {
+						m = d["data"]
+					} catch (e) {
+						ws.send(json.stringify({ok:false, code:-3, msg:"NOT_JSON5"}), pdc(error, con, ip));
+						return;
 					}
-				} else if (d["cmd"] == "ress2") {
-					if(d["data"] == undefined) {
-						ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}));
-					} else {
-						try {
-							m = d["data"]
-						} catch (e) {
-							ws.send(json.stringify({ok:false, code:-3, msg:"NOT_JSON5"}));
-							return;
-						}
-						if(m["code"] == undefined || m["password"] == undefined || m["conpass"] == undefined) {
-							ws.send(json.stringify({ok: false, code:-3, msg:"MISSING_DATA"}));
-							return
-						}
-						con.query("select uid from users where rs = ?", [m["code"]], function(a, b) {
-							if (a) throw a;
-							if (b.length != 1) {
-								ws.send(json.stringify({ok:false, code:-4, msg:"FRAUD"}));
-								console.log(ip + " tried to access a code that doesn't exist.");
-							} else {
-								if (m["password"] != m["conpass"]) {ws.send(json.stringify({ok: false, code: -4, msg: "DIFFERENT"}))}
-								else {
-									con.query("UPDATE users set password = ?, token = ?, rs = 0 where rs = ?", [pass.hash(m["password"]), uuid(30), m["code"]], function(a, b) {
-										if (a) throw a;
-										ws.send(json.stringify({ok:true, code:4, data:"LOGIN_AGAIN"}));
-									});
-								}
+					if(m["code"] == undefined || m["password"] == undefined || m["conpass"] == undefined) {
+						ws.send(json.stringify({ok: false, code:-3, msg:"MISSING_DATA"}), pdc(error, con, ip));
+						return
+					}
+					con.query("select uid from users where rs = ?", [m["code"]], function(a, b) {
+						if (a) throw a;
+						if (b.length != 1) {
+							ws.send(json.stringify({ok:false, code:-4, msg:"FRAUD"}), pdc(error, con, ip));
+							console.log(ip + " tried to access a code that doesn't exist.");
+						} else {
+							if (m["password"] != m["conpass"]) {ws.send(json.stringify({ok: false, code: -4, msg: "DIFFERENT"}), pdc(error, con, ip))}
+							else {
+								con.query("UPDATE users set password = ?, token = ?, rs = 0 where rs = ?", [pass.hash(m["password"]), uuid(30), m["code"]], function(a, b) {
+									if (a) throw a;
+									ws.send(json.stringify({ok:true, code:4, data:"LOGIN_AGAIN"}), pdc(error, con, ip));
+								});
 							}
-						});
-					}
-				} else if (d["cmd"] == "reset") {
-					if(d["data"] == undefined) {
-						ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}));
-					} else {
-							con.query("select email from users where username = ?", [d["data"].toLowerCase()], function (a, b) {
+						}
+					});
+				}
+			} else if (d["cmd"] == "reset") {
+				if(d["data"] == undefined) {
+					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}), pdc(error, con, ip));
+				} else {
+					con.query("select email from users where username = ?", [d["data"].toLowerCase()], function (a, b) {
+						if (a) throw a;
+						if (b.length != 1) {ws.send(json.stringify({ok:false, code:-3, msg:"NO_USR"}), pdc(error, con, ip))}
+						else {
+							token = uuid(30);
+							con.query("update users set rs=? where username=?", [token, d["data"]], function(a) {
 								if (a) throw a;
-								if (b.length != 1) {ws.send(json.stringify({ok:false, code:-3, msg:"NO_USR"}))}
-								else {
-									token = uuid(30);
-									con.query("update users set rs=? where username=?", [token, d["data"]], function(a) {
-										if (a) throw a;
-									});
-									sendemail(b["0"].email, "d-3fcf2355b269462cb8941330ce44175f", {username: d["data"], url: "https://loi.nayami.party/game/login?reset&code="+token});
-									ws.send(json.stringify({ok: true, code: 4, msg: "SENT_EMAIL"}));
-								}
 							});
-					}
-				} else if (d["cmd"] == "cun") {
-					if(d["data"] == undefined) {
-						ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}));
-					} else if (black.includes(d["data"].toLowerCase())) {
-						ws.send(json.stringify({ok:false, code:-4, data:"BL"}))
-					} else {
+							sendemail(b["0"].email, "d-3fcf2355b269462cb8941330ce44175f", {username: d["data"], url: "https://loi.nayami.party/game/login?reset&code="+token});
+							ws.send(json.stringify({ok: true, code: 4, msg: "SENT_EMAIL"}), pdc(error, con, ip));
+						}
+					});
+				}
+			} else if (d["cmd"] == "cun") {
+				if(d["data"] == undefined) {
+					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}), pdc(error, con, ip));
+				} else if (black.includes(d["data"].toLowerCase())) {
+					ws.send(json.stringify({ok:false, code:-4, data:"BL"}), pdc(error, con, ip))
+				} else {
 					con.query("select username from users where username = ?", [d["data"].toLowerCase()], function (a, b) {
 						if (a) throw a;
-						if (b.length > 0) {ws.send(json.stringify({ok:true, code:4, data:"F"}));}
-						else {ws.send(json.stringify({ok:true, code:4, data:"NF"}));}
+						if (b.length > 0) {ws.send(json.stringify({ok:true, code:4, data:"F"}), pdc(error, con, ip));}
+						else {ws.send(json.stringify({ok:true, code:4, data:"NF"}), pdc(error, con, ip));}
 					});
 				}
 			} else if (d["cmd"] == "cem") {
 				if(d["data"] == undefined) {
-					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}));
+					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}), pdc(error, con, ip));
 				} else {
 					con.query("select username from users where email = ?", [d["data"]], function (a, b) {
 						if (a) throw a;
-						if (b.length > 0) {ws.send(json.stringify({ok:true, code:4, data:false}));}
-						else {ws.send(json.stringify({ok:true, code:4, data:true}));}
+						if (b.length > 0) {ws.send(json.stringify({ok:true, code:4, data:false}), pdc(error, con, ip));}
+						else {ws.send(json.stringify({ok:true, code:4, data:true}), pdc(error, con, ip));}
 					});
 				}
 			} else if (d["cmd"] == "create") {
 				if(d["data"] == undefined) {
-					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}));
+					ws.send(json.stringify({ok:false, code:-3, msg:"NO_DATA_FOUND"}), pdc(error, con, ip));
 				} else {
 					n = d.data;
 					//un, em, pw, sp
 					n.un = n.un.toLowerCase();
 					n.em = n.em.toLowerCase();
 					con.query("select username from users where username = ? or email = ?", [n.un, n.em], function (a, b) {
-						if(b.length > 0) {ws.send(json.stringify({ok: false, code:6, msg: "ACCT_EXISTS"}))}
-						else if (black.includes(n.un.toLowerCase()))  {ws.send(json.stringify({ok: false, code:6, msg: "ACCT_BLACKLIST"}))}
+						if(b.length > 0) {ws.send(json.stringify({ok: false, code:6, msg: "ACCT_EXISTS"}), pdc(error, con, ip))}
+						else if (black.includes(n.un.toLowerCase()))  {ws.send(json.stringify({ok: false, code:6, msg: "ACCT_BLACKLIST"}), pdc(error, con, ip))}
 						else {
 							token = uuid(30);
-						  ce = uuid(30);
+							ce = uuid(30);
 							con.query("INSERT INTO `users`(`username`, `password`, `email`, `token`, `ce`, `spid`) VALUES (?,?,?,?,?,?);", [n.un, pass.hash(n.pw),n.em, token, ce, n.sp], function (a) {
 								if (a) throw a;
 								sendemail(n.em, "d-01419621eb244bd29bb43c34fcd6b5dd", {username: n.un, url: "https://loi.nayami.party/game/login?confirm=" + ce + "&username=" + n.un});
-								ws.send(json.stringify({ok:true, code:4, msg:"CHECK_EMAIL"}));
+								ws.send(json.stringify({ok:true, code:4, msg:"CHECK_EMAIL"}), pdc(error, con, ip));
 							});
 						}
 					});
 				}
 			}
-			});
 		});
+	});
 
 	serve.on('connection', function connection(ws, req) {
 		let ip = req.headers['x-forwarded-for']
@@ -209,11 +211,11 @@ con.connect(function(err) {
 							ok: false,
 							msg: "not_JSON5",
 							code: -2
-						}));
+						}), pdc(error, con, ip));
 						return;
 					}
 					if (x["cmd"] == undefined) {
-						ws.send(json.stringify({ ok: false, msg: "cmd_not_found", code: -1 }));
+						ws.send(json.stringify({ ok: false, msg: "cmd_not_found", code: -1 }), pdc(error, con, ip));
 						return;
 					} else {
 						if (x["cmd"] == "ddm") {
@@ -222,7 +224,7 @@ con.connect(function(err) {
 								ok: true,
 								msg: "SO_REMEMBER_ME_AND_I_WILL_REMEMBER_YOU",
 								code: 2152
-							}));
+							}), pdc(error, con, ip));
 							return;
 						} else if (x["cmd"] == "charge") {
 
@@ -238,7 +240,7 @@ con.connect(function(err) {
 								cfg.put("user." + uid + ".token", x["atoken"]);
 								cfg.put("user." + uid + ".un", b[0].username);
 								console.log(ip + " identified as " + b[0].username + ".");
-								ws.send(json.stringify({ ok: true, msg: "I_THOUGHT_I_REMEMBERED_YOU_OWO", code: 5 }));
+								ws.send(json.stringify({ ok: true, msg: "I_THOUGHT_I_REMEMBERED_YOU_OWO", code: 5 }), pdc(error, con, ip));
 							} else ws.close(1013);
 						});
 					}
@@ -257,13 +259,13 @@ con.connect(function(err) {
 			ds = 0
 			data = data.split(/\r?\n|\r/g)[0].replace(/\</g, '&lt;').replace(/\>/g, '&gt;').trim();
 			if (data.length >= 80) {
-				ws.send(json.stringify({ ok: false, display: "*Your voice falls on deaf ears. (Too many characters.)", color: "red" }));
+				ws.send(json.stringify({ ok: false, display: "*Your voice falls on deaf ears. (Too many characters.)", color: "red" }), pdc(error, con, ip));
 				ds = 1
 			}
 			if (ds == 0) {
 				if (data.split(" ")[0] == "!s") {
 					//Use shout
-					ws.send(json.stringify({ ok: false, display: "*Shouts are not setup yet.", color: "red" }));
+					ws.send(json.stringify({ ok: false, display: "*Shouts are not setup yet.", color: "red" }), pdc(error, con, ip));
 				} else if (data.split(" ")[0] == "!g") {
 					//Guilds
 				} else if (data.split(" ")[0] == "!f") {
@@ -278,30 +280,30 @@ con.connect(function(err) {
 												con.query("SELECT citid from users where token = ?", [cfg.get("user." + client._socket.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '') + ".token")], function a(e, f) {
 													uid = req.connection.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '');
 													if (e) throw e;
-													if (f.length == 1) { client.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f all ", ""), color: "pink" })); }
+													if (f.length == 1) { client.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f all ", ""), color: "pink" }), pdc(error, con, ip)); }
 												});
 											}
 										}
 									});
 								});
-								ws.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f all ", ""), color: "pink" }));
+								ws.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f all ", ""), color: "pink" }), pdc(error, con, ip));
 							});
 						});
 					} else {
 						fun = data.split(" ")[1].toLowerCase();
-						if (fun == cfg.get("user." + uid + ".un")) { ws.send(json.stringify({ ok: false, display: "You cannot send a message to yourself." })) } else {
+						if (fun == cfg.get("user." + uid + ".un")) { ws.send(json.stringify({ ok: false, display: "You cannot send a message to yourself." }), pdc(error, con, ip)) } else {
 
 							con.query("select uid from users where token = ?", [cfg.get("user." + uid + ".token")], function(f, g) {
 								con.query("select token from users where username = ? and uid in ( select ut from friends where uf = ? ) or uid in ( select uf from friends where ut = ? )", [fun, g[0].uid, g[0].uid], function(a, b) {
-									if (b.length == 0) { ws.send(json.stringify({ ok: false, display: fun + " is not a friend of yours. Are you sure you typed their name correctly?" })) } else {
+									if (b.length == 0) { ws.send(json.stringify({ ok: false, display: fun + " is not a friend of yours. Are you sure you typed their name correctly?" }), pdc(error, con, ip)) } else {
 										chat.clients.forEach(function each(client) {
 											if (client.readyState === WebSocket.OPEN) {
 												con.query("SELECT citid from users where token = ?", [cfg.get("user." + client._socket.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '') + ".token")], function a(a, e) {
 													uid = req.connection.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '');
 													if (a) throw a;
 													if (b.length == 1) {
-														client.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f " + fun + " ", ""), color: "pink" }));
-														ws.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f " + fun + " ", ""), color: "pink" }));
+														client.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f " + fun + " ", ""), color: "pink" }), pdc(error, con, ip));
+														ws.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data.replace("!f " + fun + " ", ""), color: "pink" }), pdc(error, con, ip));
 													}
 												});
 											}
@@ -319,7 +321,7 @@ con.connect(function(err) {
 							con.query("SELECT citid from users where token = ?", [cfg.get("user." + client._socket.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '') + ".token")], function a(a, b) {
 								uid = req.connection.remoteAddress.replace(/::ffff:/g, '').replace(/\./g, '');
 								if (a) throw a;
-								if (b.length == 1) { client.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data })); }
+								if (b.length == 1) { client.send(json.stringify({ ok: true, display: cfg.get("user." + uid + ".un") + ">> " + data }), pdc(error, con, ip)); }
 							});
 						}
 					});
@@ -331,7 +333,7 @@ con.connect(function(err) {
 
 	battle.on('connection', function connection(ws, req) {
 		isconnected(req, ws);
-		if(cnf.get("user."+uid+"battleid")) {ws.send(json.stringify({ok:true, code:2, bid: cnf.get("user."+uid+"battleid"), msg:"YOU_ARE_STILL_IN_A_FIGHT"}));}
+		if(cnf.get("user."+uid+"battleid")) {ws.send(json.stringify({ok:true, code:2, bid: cnf.get("user."+uid+"battleid"), msg:"YOU_ARE_STILL_IN_A_FIGHT"}), pdc(error, con, ip));}
 		else {
 			r = Math.random().toString(36).substring(7);
 		}
