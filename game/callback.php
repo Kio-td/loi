ba25996d20874015aa1d1e67cd4ccc31245f0cdc
Kio-td <toobanooba@gmail.com>
2018-10-22 18:23:09 -0400

    Begin working on an Advertising Callback

---
+++ game/callback.php
@@ -0,0 +1,39 @@
+<?php
+    echo "<style>input{color: white !important;}</style>";
+    require('../base/head.php');
+?>
+</nav>
+</div>
+</header>
+<main role="main" class="inner cover">
+
+  <h1 class="cover-heading">Login</h1>
+  <p class="lead">Welcome back. Login to continue your adventure. </p>
+    <input class="form-control" id="un" placeholder="Username" required type="text"><br>
+    <input class="form-control" id="pw" placeholder="Password" required type="password"><br>
+    <button onclick="auth()" class="btn btn-secondary">Login</button>
+
+    <script>
+      s = new WebSocket("wss://ws.nayami.party/anon");
+      function auth() {
+        s.onmessage = function(evt) {
+          data = json.parse(evt.data);
+          if(data.ok == false) {
+            if(data.msg == "CONF_EMAIL") {err("Please look in your inbox for the confirmation email.")}
+            else if (data.msg == "INC_PASS") {err("Your username or password is incorrect.")}
+            else if (data.msg == "NOBODY_FOUND") {err("Your username or password is incorrect.")}
+          } else {
+            document.cookie = "token=" + data.data;
+            window.location = "index";
+          }
+        }
+        s.send(json.stringify({cmd:'auth', data:{un:document.getElementById('un').value, pw:document.getElementById('pw').value}}))
+      }
+    </script>
+    <?php
+
+    }
+
+    require('../base/feet.php');
+}
+?>
