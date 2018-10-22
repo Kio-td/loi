73a6c5fcd0a9f4ff5cee3b174e7082b21c78a43b
Kio-td <toobanooba@gmail.com>
2018-10-22 19:12:11 -0400

    Forgot to add quotes. -w-

--- game/callback.php
+++ game/callback.php
@@ -37,7 +37,7 @@
             else if (data.msg == "INC_PASS") {err("Your username or password is incorrect.")}
             else if (data.msg == "NOBODY_FOUND") {err("Your username or password is incorrect.")}
           } else {
-            window.location = <?php echo urlencode($_GET["return"]); ?> + "?token="+data.token+"&username="+data.username;
+            window.location = "<?php echo $_GET["return"]; ?>" + "?token="+data.token+"&username="+data.username;
           }
         }
         s.send(json.stringify({cmd:'authcallback', data:{un:document.getElementById('un').value, pw:document.getElementById('pw').value}}))
