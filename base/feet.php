a69a5d35484e588c6fdaea10854dd97c01c29b43
Kio-td <toobanooba@gmail.com>
2018-10-25 15:20:51 -0400

    Use a real, actual Devtool finder.

--- base/feet.php
+++ base/feet.php
@@ -16,14 +16,14 @@
 
 <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/jdetects@0.1.0/jdetects.js">
 <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });
-var devtools = /./;
-devtools.toString = function() {
-  this.opened = true;
-}
-while(devtools.opened == true) {
+
+jdetects.create(function(status) {
   console.error("!!!GAME MAYBE TAMPERED WITH NOW!!!\nI have closed all connections. Please close DevConsole and restart the game.")
-}
+
+});
+
 </script>
 
 
