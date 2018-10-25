b931394d69d9aae624cd05d7cf683468b9384c2d
Kio-td <toobanooba@gmail.com>
2018-10-25 15:25:33 -0400

    Ehh

--- base/feet.php
+++ base/feet.php
@@ -19,13 +19,13 @@
 <script src="https://unpkg.com/jdetects@0.1.0/jdetects.js">
 <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });
 var t = false
-jdetects.create(function(status) {
-  t = status
+jdetects.create({
+	once: true,
+	onchange: function(status) {
+    console.clear();
+    console.error("!!!GAME MAYBE TAMPERED WITH NOW!!!\nI have closed all connections. Please close DevConsole and restart the game.")
+	}
 });
-while (t) {
-  console.clear();
-  console.error("!!!GAME MAYBE TAMPERED WITH NOW!!!\nI have closed all connections. Please close DevConsole and restart the game.")
-}
 
 </script>
 
