3b4fb449d48ae1861de0294ad85928e8db537a42
Kio-td <toobanooba@gmail.com>
2018-10-25 15:08:32 -0400

    Alter the head and feet to allow JQury to properly load

--- base/head.php
+++ base/head.php
@@ -34,7 +34,8 @@ $x = false;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Team LOI">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/json5/0.5.1/json5.min.js"></script>
     <script>
     json = JSON5;
