936badbbb306d3a012043db5d37944311c42ded4
Kio-td <toobanooba@gmail.com>
2018-10-25 14:23:18 -0400

    Color the buttons, and make them be seen as buttons.

--- game/battle.php
+++ game/battle.php
@@ -13,13 +13,13 @@ require('../base/head.php');
   <div class="inv">
   </div>
   <div class="rab">
-    <div class="atk">
+    <div class="atk btn btn-danger">
     </div>
-    <div class="def">
+    <div class="def btn btn-info">
     </div>
-    <div class="inv">
+    <div class="inv btn btn-primary">
     </div>
-    <div class="fle">
+    <div class="fle btn btn-warning">
     </div>
   </div>
   <div class="trip">
