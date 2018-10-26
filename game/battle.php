13b78f57dbc124a1951e6288c2c91cfee893819c
Kio-td <toobanooba@gmail.com>
2018-10-26 18:46:54 -0400

    Add in the count.

--- game/battle.php
+++ game/battle.php
@@ -1,3 +1,20 @@
+<?php
+$auth = false;
+if (isset($_COOKIE["token"])) {
+  require("/var/www/no-access/loi/config.php");
+  $n = $conn->escape_string(base64_decode($_COOKIE["token"]));
+  $c = $conn->query("SELECT bal, token, username from users where token = '".$n."'");
+  if (!$c->num_rows) {
+    $na = array("index.php", "login.php", "index", "login", "login-");
+} else {
+  $auth = true;
+  $c = $c->fetch_assoc();
+  $username = ucfirst($c["username"]);
+  $bal = $c["bal"];
+}
+}
+$x = false;
+?>
 <html lang="en">
 <head>
   <meta charset="utf-8">
