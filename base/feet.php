a8a9d89c7a3dc522a2c8d784107ea4e38b3529a5
Kio-td <toobanooba@gmail.com>
2018-10-25 21:16:56 -0400

    Move the scripts to the head, because it's just a pain to load these.

--- base/feet.php
+++ base/feet.php
@@ -13,9 +13,6 @@
 <?php
 }
 ?>
-
-<script src="https://unpkg.com/tooltip.js/dist/umd/tooltip.min.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/jdetects@0.1.0/jdetects.js"></script>
 <script>
 $(document).ready(function() { $('body').bootstrapMaterialDesign(); });
