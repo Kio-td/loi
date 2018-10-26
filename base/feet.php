2e0c68507bd802cd6352df9dbaac24a553e19f9d
Kio-td <toobanooba@gmail.com>
2018-10-26 04:46:42 -0400

    Move items around to become more compatible.

--- base/feet.php
+++ base/feet.php
@@ -13,6 +13,26 @@
 <?php
 }
 ?>
+<script>
+function err(info) {
+document.getElementById('ap').innerHTML += '<div class="alert alert-warning alert-dismissible fade show" role="alert">'+info+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+}
+function inf(info) {
+document.getElementById('ap').innerHTML += '<div class="alert alert-info alert-dismissible fade show" role="alert">'+info+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+}
+function suc(info) {
+document.getElementById('ap').innerHTML += '<div class="alert alert-success alert-dismissible fade show" role="alert">'+info+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+}
 
+$(document).ready(function() { $('body').bootstrapMaterialDesign(); });
+var t = false
+jdetects.create({
+once: true,
+onchange: function(status) {
+console.clear();
+console.error("!!!GAME MAYBE TAMPERED WITH NOW!!!\nI have closed all connections. Please close DevConsole and restart the game.");
+}
+});
+</script>
 
 </body></html>
