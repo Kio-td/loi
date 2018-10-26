188a14ec6c64fb93133c43b63ba2a02f328e6e31
Kio-td <toobanooba@gmail.com>
2018-10-26 05:42:38 -0400

    move the priorities around.

--- base/head.php
+++ base/head.php
@@ -38,12 +38,12 @@ $x = false;
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="Team LOI">
-    <script src="/assets/bootstrap-material-design.min.js"></script>
-    <script src="/assets/jdetects.min.js"></script>
     <script src="/assets/jquery.slim.min.js"></script>
+    <script src="/assets/jdetects.min.js"></script>
     <script src="/assets/json5.min.js"></script>
     <script src="/assets/popper.min.js"></script>
     <script src="/assets/tooltip.min.js"></script>
+    <script src="/assets/bootstrap-material-design.min.js"></script>
     <script>
       json = JSON5;
     </script>
