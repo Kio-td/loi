5a2d0c65b2176fd0a4e857245c186d35bb813781
Kio-td <toobanooba@gmail.com>
2018-10-26 05:48:38 -0400

    Stop npm-ing.

--- pull.php
+++ pull.php
@@ -1,5 +1,4 @@
 <?php
 
 echo shell_exec("cd /var/www/loi && /usr/bin/git pull 2>&1");
-echo shell_exec("cd /var/www/loi && /usr/bin/npm install");
 echo shell_exec("cd /home/kio_thedev_gmail_com/loi && /usr/bin/git pull 2>&1");
