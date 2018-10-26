21100d28c720d9ca4c9ada3df6bda877c5ab7434
Kio-td <toobanooba@gmail.com>
2018-10-25 20:59:30 -0400

    BR's don't work.

--- game/battle.php
+++ game/battle.php
@@ -19,7 +19,6 @@ require('../base/head.php');
   </div>
   <div class="">
   </div>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
   </div>
 
   <div class="inv">
