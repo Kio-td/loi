a31a67b5a45aae489e9dda52d71c045be63a3a18
Kio-td <toobanooba@gmail.com>
2018-10-30 15:24:21 -0400

    Let's see if this'll do

--- base/head.php
+++ base/head.php
@@ -2,20 +2,6 @@
 header("X-Frame-Options: Deny");
 header("X-XSS-Protection: 1; mode=block");
 header("X-Content-Type-Options: nosniff");
-function sendmail($sub, $mail, $user, $body) {
-require '/var/www/loi/vendor/autoload.php';
-$email = new \SendGrid\Mail\Mail();
-$email->setFrom("Noreply@loi.nayami.party", "Legend of Ikaros");
-$email->setSubject($sub);
-$email->addTo($mail, $user);
-$email->addContent("text/plain", $body);
-$sendgrid = new \SendGrid("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");
-try {
-    $response = $sendgrid->send($email);
-} catch (Exception $e) {
-    echo 'We were unable to send your email. Please, notify Kio.';
-}
-}
 $auth = false;
 if (isset($_COOKIE["token"])) {
   require("/var/www/no-access/loi/config.php");
