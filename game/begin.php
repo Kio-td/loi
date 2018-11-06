e3a1390ba373e85715f0469bc0aa96e27bb4e8d5
Kio-td <toobanooba@gmail.com>
2018-11-05 20:58:05 -0500

    Remove some of the filler stuff in the scripts.

--- game/begin.php
+++ game/begin.php
@@ -55,55 +55,6 @@
   <script src="../assets/bootstrap-material-design.min.js"></script>
   <script src="../assets/howler.min.js"></script>
   <script src="../assets/typeit.min.js"></script>
-  <script>
-  $('.writingbox').hide();
-   $("body").css("background", "black");
-  $('.writingbox').delay(2200).fadeIn();
-  setTimeout(function(){ document.title = "Interesting." }, 2200);
-  $(".btn").hide();
-  var beep = new Howl({volume: 0.05, src: ["../assets/beep.ogg"]});
-  var underworld = new Howl({src: ["../assets/underworld.mp3"], autoplay: true, loop: false,});
-    x = 0;
-    y = 1;
-function t (s,q,i) {
-  console.log(s);
-    if(s[2] == "first-of-string") {x = 1; y += 1;}
-    if(s[2] == "last-of-string") {x = 2}
-    if(x) {
-      if (x == 2) {x = 0}
-      if (y == 2 || y == 0) {y = 0; beep.play()}
-    }
-  }
-  function o () {
-    $(".spritebox").fadeOut();
-    $(".writingbox").fadeOut();
-    $(".btn").fadeIn();
-  }
-  new TypeIt('.writingbox', {speed: 90, afterComplete: function(s,q,i) {o()}, afterStep: function (s,q,i) {t(s,q,i)}})
-  .pause(3000)
-  .type("Interesting.")
-  .pause(500)
-  .break()
-  .type("You think you're special.")
-  .pause(500)
-  .break()
-  .type("You think you can save her.")
-  .pause(1500)
-  .empty()
-  .type("...")
-  .pause(1500)
-  .empty()
-  .type("I hope you understand that the rules apply only to you.")
-  .pause(500)
-  .break()
-  .pause(1500)
-  .options({speed: 400})
-  .type("Not to me.")
-  .pause(1000);
-
-
-  $(document).ready(function() { $('body').bootstrapMaterialDesign(); });
-
-  </script>
+  <script src="../assets/begin.min.js"></script>
 </body>
 </html>
