5ad656cd24bcf1e216e14704aa0c57545118ee58
Kio-td <toobanooba@gmail.com>
2018-11-06 11:31:54 -0500

    Send a message when the battlesocket is done.

--- game/battle.php
+++ game/battle.php
@@ -116,8 +116,7 @@ var back = "451 Char MAX.";
   }
   x = 0
   var r = new WebSocket("wss://ws.nayami.party/main");
-  r.onmessage = function(s){console.log(s);}
-  r.send(json.stringify({atoken: "<?php echo $_COOKIE["token"]; ?>" }))
+  r.onmessage = function(s){console.log(s);  r.send(json.stringify({atoken: "<?php echo $_COOKIE["token"]; ?>" }));}
   var s = new WebSocket("wss://ws.nayami.party/btl");
   s.onmessage = function (data) {
     console.log(data);
