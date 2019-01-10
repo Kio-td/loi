93075c176dae020e54544e92655903322900d9b9
Kio <toobanooba@gmail.com>
2019-01-10 16:24:56 -0500

    Create alink.js

---
+++ assets/alink.js
@@ -0,0 +1,7 @@
+/* <a href> tags will be rewritten into <a link> tags, so that the server can know what's going on.
+*  This can also improve ability to track the user. */
+
+$("a").addEventListener("click", function() {
+  s.send(json.send({cmd:}))
+  s.onClose = void
+})
