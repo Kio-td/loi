03942bf1e44870e8aa5fd27960f01725147e0eb5
Kio-td <toobanooba@gmail.com>
2019-01-11 17:37:10 -0500

    aaa

--- game/login.php
+++ game/login.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-if (filter_input(INPUT_GET,"confirm")) {
+if (isset($_GET["confirm"])) {
   if(filter_input(INPUT_GET,"username") == false || filter_input(INPUT_GET,"confirm") == false) {
     header("location: login");
     die();
