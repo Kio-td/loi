e98ea46d48bae23b2707d370a90f0b28eb0855a0
Kio-td <toobanooba@gmail.com>
2019-01-24 09:49:24 -0500

    A I am understanding the path of explosions.

--- assets/warningDialog.js
+++ assets/warningDialog.js
@@ -1,9 +1,1 @@
-function err(info) {
-document.getElementById('ap').innerHTML += '<div class="alert alert-warning alert-dismissible fade show" role="alert">'+info+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
-}
-function inf(info) {
-document.getElementById('ap').innerHTML += '<div class="alert alert-info alert-dismissible fade show" role="alert">'+info+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
-}
-function suc(info) {
-document.getElementById('ap').innerHTML += '<div class="alert alert-success alert-dismissible fade show" role="alert">'+info+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
-}
+function err(info){document.getElementById("ap").innerHTML+='<div class="alert alert-warning alert-dismissible fade show" role="alert">${info}<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'}function inf(info){document.getElementById("ap").innerHTML+='<div class="alert alert-info alert-dismissible fade show" role="alert">\'${info}\'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'}function suc(info){document.getElementById("ap").innerHTML+='<div class="alert alert-success alert-dismissible fade show" role="alert">\'${info}\'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'}
